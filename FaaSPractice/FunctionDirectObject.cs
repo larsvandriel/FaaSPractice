85442a2ce5adf771d632b908c01de318a733c2b6
larsvandriel <lars.van.driel@hotmail.com>
2022-06-23 10:26:13 +0200

    Initial commit

---
+++ FaaSPractice/FunctionDirectObject.cs
@@ -0,0 +1,47 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace FaaSPractice
+{
+    public static class FunctionDirectObject
+    {
+        [FunctionName("DirectObject")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            List<string> directObjects = new List<string>()
+            {
+                "the server",
+                "a monitor",
+                "the exams",
+                "the party"
+            };
+            
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            Random random = new Random();
+            string directObject = directObjects[random.Next(directObjects.Count)];
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(directObject);
+        }
+    }
+}
---
+++ FaaSPractice/FunctionSubject.cs
@@ -0,0 +1,47 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace FaaSPractice
+{
+    public static class FunctionSubject
+    {
+        [FunctionName("Subject")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            List<string> subjects = new List<string>()
+            {
+                "The programmer",
+                "A computer",
+                "The gouvernment",
+                "A teacher at Fontys"
+            };
+            
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            Random random = new Random();
+            string subject = subjects[random.Next(subjects.Count)];
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(subject);
+        }
+    }
+}
